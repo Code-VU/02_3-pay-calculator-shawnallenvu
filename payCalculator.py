ff21b70d5c35a03abc85c3a7e0e323a1501332d6
Shawn Allen <Shawn.Allen@vu.com>
2025-02-28 10:24:19 -0600

    Converted inputs to floats and printed gross pay

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,12 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = float(input("Enter Hours: "))
+    hourly_pay = float(input("Enter your hourly rate: "))
+
+    gross_pay = hourly_pay * hrs
+
+    print(gross_pay)
 
     # end assignment
 
